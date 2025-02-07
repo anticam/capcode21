197905cdd29383b3f3965430651c6e20c2af6942
Antal P. <antal.perger@gmail.com>
2025-02-07 23:53:53 +0100

    action fixed

--- srv/incrementLogic.cds
+++ srv/incrementLogic.cds
@@ -3,4 +3,5 @@ using { india.db.master } from '../db/datamodel';
 service increment {
     entity Worker as projection on master.worker;
     action hike( ID: UUID );
+    action hike2(ID : UUID);
 }
--- srv/incrementLogic.js
+++ srv/incrementLogic.js
@@ -1,31 +1,34 @@
-const cdsServe = require("@sap/cds/lib/srv/cds-serve");
+const cds = require("@sap/cds");
 
-module.exports = cds.service.impl(async (srv) => {
-    const { worker } = cds.entities ;
+// see https://github.com/SAP-samples/cloud-cap-samples/blob/23bea0f62947ff6c0d1908e7edf4359b328ee380/reviews/srv/reviews-service.js
+module.exports = cds.service.impl(function () {
+    const { worker } = this.entities('india.db.master');
+    //const db = cds.connect.to('db');
+    
     // const { worker } = this.entities;
 
-    srv.on('hike', async (req) => {
+    this.on('hike', async (req) => {
         const { ID } = req.data;
 
-        if (!ID) {
-            return req.reject(400, "ID is required");
-        }
+        if (!ID) { return req.reject(400, "ID is required"); }
+        
         console.log(`Received request to increment salary for Worker with ID : ${ID}`);
 
-        // Start new transaction
-        const tx = cds.transaction(req);
-        try {
+        await cds.tx(async tx => {
             // Retrieve the current salary amount of Worker
-            const workers = await tx.read(worker).where({ 'ID': ID });
+            let workers = await tx.read(worker).where({ 'ID': ID });
+
+            // Check if Worker exists
             if (!workers.length) {
                 await tx.rollback();
                 return req.reject(404, `Worker with ID ${ID} not found`);
             }
 
+            // Increment salary by 10%
             const currentSalary = workers[0].salaryAmount;
             console.log(`Current salary of Worker with ID ${ID} is ${currentSalary}`);
 
-            // Increment salary by 10%
+
             // Update operation
             const result = await tx.update(worker)
                 .set({ salaryAmount: currentSalary + 20000 })
@@ -36,19 +39,12 @@ module.exports = cds.service.impl(async (srv) => {
                 return req.reject(500, `Failed to increment salary for Worker with ID ${ID}`);
             }
 
-            // Commit the transaction
-            await tx.commit();
-            console.log(`Salary incremented successfully for Worker with ID ${ID}`);
-            return req.reply(200, `Salary incremented successfully for Worker with ID ${ID}`);
-        } catch (error) {
-            console.error(`Error occurred while incrementing salary for Worker with ID ${ID}`);
-            try {
-                await tx.rollback();
-            } catch (rollbackError) {
-                console.error(`Error occurred while rolling back transaction for Worker with ID ${ID}`);
-            }
-            
+        }).catch(async err => {
+            console.error(err);
+            await tx.rollback();
             return req.reject(500, `Error occurred while incrementing salary for Worker with ID ${ID}`);
-        }
+        });
+        
     });
-})
+});
+
